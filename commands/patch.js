2c3c4d036ca5d44520f903da3f66ad8eca2309fb
magz1k <46112988+magz1k@users.noreply.github.com>
2019-04-17 12:42:27 +0300

    Update patch.js

--- commands/patch.js
+++ commands/patch.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    message.channel.send("heyo")
+    message.channel.send("hmm")
 }
 
 module.exports.help = {
